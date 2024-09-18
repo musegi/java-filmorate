409cf50e5e22bbbe7dda08db36b632512cf0075f
musegi <marianna.reznik.00@gmail.com>
2024-09-18 22:50:37 +0300

    исправить Тесты Postman

--- src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,19 +12,25 @@ public class ErrorHandler {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler
-    public ErrorResponse handleNotFoundException(NotFoundException e) {
-        return new ErrorResponse("Объект не найден.", e.getMessage());
+    public ErrorResponse handleNotFoundException(NotFoundException exp) {
+        return new ErrorResponse("Объект не найден.", exp.getMessage());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler
-    public ErrorResponse handleValidationException(ValidationException e) {
-        return new ErrorResponse("Ошибка валидации.", e.getMessage());
+    public ErrorResponse handleValidationException(ValidationException exp) {
+        return new ErrorResponse("Ошибка валидации.", exp.getMessage());
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler
-    public ErrorResponse handleException(Exception e) {
-        return new ErrorResponse("Произошла непредвиденная ошибка.", e.getMessage());
+    public ErrorResponse handleException(Exception exp) {
+        return new ErrorResponse("Произошла непредвиденная ошибка.", exp.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler
+    public ErrorResponse handleMethodArgumentNotValidException (MethodArgumentNotValidException exp) {
+        return new ErrorResponse("Ошибка валидации.", exp.getMessage());
     }
 }
